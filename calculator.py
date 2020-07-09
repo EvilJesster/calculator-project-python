a7e4b82d52d2bee16907f5c90be488af6ba990ed
Jesse Hall <jesse.hall@cooper.edu>
2020-07-09 09:54:39 -0400

    start of day 2

--- calculator.py
+++ calculator.py
@@ -3,3 +3,17 @@ import definitions
 
 print("Welcome to the Python Calculator\n\nWhat would you like to do?")
 choice = input()
+
+if choice == "add":
+    print(definitions.add())
+elif choice == 'subtract':
+    print(definitions.subtract())
+elif choice == 'multiply':
+    print(definitions.multiply())
+elif choice == 'divide':
+    print(definitions.divide())
+elif choice == 'simple':
+    print(definitions.simple())
+
+
+
--- definitions.py
+++ definitions.py
@@ -1,1 +1,43 @@
+
+
 # Feel free to code your definitions here in order to separate your concerns.
+num1 = 0
+num2 = 0
+
+def get_numbers():
+    global num1, num2
+    num1 = float(input("What is the first number? "))
+    num2 = float(input("What is the second number? "))
+
+
+def add():
+    global num1, num2
+    get_numbers()
+    sum = num1 + num2
+    print(str(num1) + " + " + str(num2) + " = " + str(sum))
+
+
+
+def subtract():
+    global num1, num2
+    get_numbers()
+    sum = num1 - num2
+    return(str(num1) + " - "+ str(num2) + " = " + str(sum))
+
+def multiply():
+    global num1, num2
+    get_numbers()
+    sum = num1 * num2
+    return(str(num1) + " * "+ str(num2) + " = " + str(sum))
+
+def divide():
+    global num1, num2
+    get_numbers()
+    if(num1 == 0 or num2 == 0):
+        return("No singularities please thank you")
+    sum = num1 / num2
+    return(str(num1) + " / "+ str(num2) + " = " + str(sum))
+
+def simple():
+    runthis = input('input math normally, for ex 4 + 4')
+    return(eval(runthis))
